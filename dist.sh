816045e1ff3b3d69fd9ea5005b8bf7206aa4be94
Pasi Niemi <pasi.niemi@nitor.com>
2021-04-22 22:01:39 +0300

    Cut & paste fixes

--- dist.sh
+++ dist.sh
@@ -4,6 +4,6 @@ VERSION=$1
 MESSAGE="$2"
 bumpversion --new-version $VERSION --message "$MESSAGE" setup.py
 python setup.py sdist bdist_wheel
-gpg -o dist/adfs_aws_login-${VERSION}-py2.py3-none-any.whl.asc -a -b dist/adfs_aws_login-${VERSION}-py2.py3-none-any.whl
-gpg -o dist/adfs-aws-login-${VERSION}.tar.gz.asc -a -b dist/adfs-aws-login-${VERSION}.tar.gz
+gpg -o dist/lastpass_aws_login-${VERSION}-py2.py3-none-any.whl.asc -a -b dist/lastpass_aws_login-${VERSION}-py2.py3-none-any.whl
+gpg -o dist/lastpass-aws-login-${VERSION}.tar.gz.asc -a -b dist/lastpass-aws-login-${VERSION}.tar.gz
 twine upload dist/*
